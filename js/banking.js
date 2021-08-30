8f7d811b9ee9f68ca66fc2ff3d883146fc4c5d21
RubelHasan <rubelhasan306@gmail.com>
2021-08-31 02:03:50 +0600

    added input validation

--- js/banking.js
+++ js/banking.js
@@ -6,26 +6,33 @@ document.getElementById('deposit').addEventListener('click', function(){
     const depositInputText = depositInput.value;
     const depositInputAmount = parseFloat(depositInputText);
 
-    //current deposit amount 
-    const prevDeposit = document.getElementById('deposit-current');
-    const prevDepositText = prevDeposit.innerText;
-    const prevDepositAmount = parseFloat(prevDepositText);
+    //added some validation 
+    if(depositInputText != ''){
+        //current deposit amount 
+        const prevDeposit = document.getElementById('deposit-current');
+        const prevDepositText = prevDeposit.innerText;
+        const prevDepositAmount = parseFloat(prevDepositText);
 
-    //update deposit amount
-    const totalDepositAmount = prevDepositAmount + depositInputAmount;
-    prevDeposit.innerText = totalDepositAmount;
+        //update deposit amount
+        const totalDepositAmount = prevDepositAmount + depositInputAmount;
+        prevDeposit.innerText = totalDepositAmount;
 
-    //current balance amount 
-    const prevBalance = document.getElementById('balance-current');
-    const prevBalanceText = prevBalance.innerText;
-    const prevBalanceAmount = parseFloat(prevBalanceText);
+        //current balance amount 
+        const prevBalance = document.getElementById('balance-current');
+        const prevBalanceText = prevBalance.innerText;
+        const prevBalanceAmount = parseFloat(prevBalanceText);
 
-    //update balance amount 
-    const totalBalanceAmount = prevBalanceAmount + depositInputAmount;
-    prevBalance.innerText = totalBalanceAmount;
+        //update balance amount 
+        const totalBalanceAmount = prevBalanceAmount + depositInputAmount;
+        prevBalance.innerText = totalBalanceAmount;
 
-    //clear deposit amount 
-    depositInput.value = ''
+        //clear deposit amount 
+        depositInput.value = '';
+    }else{
+        alert("Please add some amount !!")
+    }
+
+   
 })
 
 //working for withdraw amount 
@@ -37,28 +44,33 @@ document.getElementById('withdraw').addEventListener('click', function(){
     let withdrawInputText = withdrawInput.value;
     let withdrawInputAmount = parseFloat(withdrawInputText);
    
+    //added validation for empty value 
+    if(withdrawInputText != ''){
+        // current withdraw amount 
+        let prevWithdraw = document.getElementById('withdraw-current');
+        let prevWithdrawText = prevWithdraw.innerText;
+        let prevWithdrawAmount = parseFloat(prevWithdrawText);
 
-    // current withdraw amount 
-    let prevWithdraw = document.getElementById('withdraw-current');
-    let prevWithdrawText = prevWithdraw.innerText;
-    let prevWithdrawAmount = parseFloat(prevWithdrawText);
+        //update withdraw amount 
+        let totalWithdrawAmount = prevWithdrawAmount + withdrawInputAmount;
+        prevWithdraw.innerText = totalWithdrawAmount;
 
-    //update withdraw amount 
-    let totalWithdrawAmount = prevWithdrawAmount + withdrawInputAmount;
-    prevWithdraw.innerText = totalWithdrawAmount;
+        // update Balance after withdrawn 
+        //current balance amount 
+        const prevBalance = document.getElementById('balance-current');
+        const prevBalanceText = prevBalance.innerText;
+        const prevBalanceAmount = parseFloat(prevBalanceText);
 
-    // update Balance after withdrawn 
-     //current balance amount 
-     const prevBalance = document.getElementById('balance-current');
-     const prevBalanceText = prevBalance.innerText;
-     const prevBalanceAmount = parseFloat(prevBalanceText);
-
-     //new balance 
-     const newTotalBalance = prevBalanceAmount - withdrawInputAmount;
-     prevBalance.innerText = newTotalBalance;
+        //new balance 
+        const newTotalBalance = prevBalanceAmount - withdrawInputAmount;
+        prevBalance.innerText = newTotalBalance;
 
 
-    //clear withdraw input field 
-    withdrawInput.value = '';
+        //clear withdraw input field 
+        withdrawInput.value = '';
+    }else{
+        alert("Please enter amount you want to withdraw !!")
+    }
+    
     
 })
